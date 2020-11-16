6ae68e38a0a1eda3abeaffde690dbf40799c2743
Lazar <46060203+Lazzaro98@users.noreply.github.com>
2020-11-16 04:37:39 +0100

    Add files via upload

--- SubtitleEditor.cpp
+++ SubtitleEditor.cpp
@@ -77,3 +77,105 @@ string SubtitleEditor::saveSubtitle() {
 	ret += "\n";
 	return ret;
 }
+
+void SubtitleEditor::addToSelection(int start,int end) {
+	if (start < 1) start = 1;
+	if (end > posl->id)end = posl->id;
+
+	Elem* pom = prvi;
+	while (pom) {
+		if (pom->id >= start && pom->id <= end) pom->selected = true;
+		pom = pom->next;
+	}
+}
+
+void SubtitleEditor::deselect() {
+	Elem* pom = prvi;
+	while (pom) {
+		if(pom->selected)pom->selected = false;
+		pom = pom->next;
+	}
+}
+
+string SubtitleEditor::fontTekst(string tekst,int start, int end,char f) {
+	//Ja sam <b>car</b> lazar , start=7 end 10
+	//if (tekst == "")return tekst;
+	if (start < 0) start = 0;
+	if (end > tekst.length() - 1)end = tekst.length() - 1;
+	
+	string noviTekst = "";
+	string openTag = "<" + string(1, f); openTag += ">";
+	string closeTag = "</" + string(1, f); closeTag += ">";
+	for (int i = 0; i <= start; i++)noviTekst += tekst[i]; //Ja sam 
+	noviTekst += openTag; //Ja sam <b> 
+	for (int i = start + 1; i < end + 1; i++)noviTekst += tekst[i];//Ja sam <b> car 
+	noviTekst += closeTag;
+	for (int i = end + 1; i < (tekst.length() + 1); i++)noviTekst += tekst[i];
+	return noviTekst;
+}
+void SubtitleEditor::fontOpseg(int start, int end,char f) {
+	Elem* pom = prvi;
+	while (pom) { // prolazim kroz listu i font-ujem selektovane
+		if (pom->selected) {
+			pom->tekst = fontTekst(pom->tekst,start,end,f);
+		}
+		pom = pom->next;
+	}
+}
+
+
+void SubtitleEditor::boldText(int start, int end) {
+	fontOpseg(start, end, 'b');
+}
+void SubtitleEditor::italicText(int start, int end) {
+	fontOpseg(start, end, 'i');
+}
+void SubtitleEditor::underlineText(int start, int end) {
+	fontOpseg(start, end, 'u');
+}
+string SubtitleEditor::removeStyleText(string tekst) {
+	string buf = tekst;
+	int idx = 0;
+	int opened = 0;
+	for (int i = 0; i < buf.length(); i++) {
+		if (buf[i] == '<') opened = 1;
+		else if (buf[i] == '>')opened = 0;
+		else if (!opened)buf[idx++] = buf[i];
+	}
+	string ret = "";
+	for (int i = 0; i < idx; i++)ret += buf[i];
+	return ret;
+}
+void SubtitleEditor::removeStyle() {
+	Elem* pom = prvi;
+	while (pom) {
+		if (pom->selected) {
+			pom->tekst = removeStyleText(pom->tekst);
+		}
+		pom = pom->next;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- SubtitleEditor.h
+++ SubtitleEditor.h
@@ -4,12 +4,17 @@
 using namespace std;
 class SubtitleEditor
 {
-protected:
+private:
+	string bolduj(string tekst,int start,int end);
+	string iskrivi(string tekst,int start,int end);
+	string podvuci(string tekst, int start, int end);
+public:
 	struct Elem {
 		int id;
 		Time* startTime;
 		Time* endTime;
 		string tekst;
+		bool selected;
 		Elem* next;
 		Elem(int id1, Time* startTime1, Time* endTime1, string tekst1, Elem* next1=nullptr) {
 			this->id = id1;
@@ -17,16 +22,25 @@ protected:
 			this->endTime = endTime1;
 			this->tekst = tekst1;
 			this->next = next1;
+			this->selected = false;
 		}
 	};
-public:
+
 	Elem* prvi, * posl;
 	SubtitleEditor() {}
 	~SubtitleEditor() {}
 
 	int loadSubtitle(string subtitle);
 	string saveSubtitle();
-
+	void addToSelection(int start, int end);
+	void deselect();
+	string fontTekst(string tekst, int start, int end, char f);
+	void fontOpseg(int start, int end, char f);
+	void boldText(int start, int end);
+	void italicText(int start, int end);
+	void underlineText(int start, int end);
+	void removeStyle();
+	string removeStyleText(string tekst);
 	void dodajNaListu(Elem* nov);
 	int isNumber(char c);
 };
